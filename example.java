5100faf32ecc92e7777eec21b1fad6788c1d175d
sirboz0 <mohd.psn937@gmail.com>
2026-01-07 14:29:12 +0300

    added example starter code

---
+++ example.java
@@ -0,0 +1,26 @@
+package CoffeeLib;
+
+import CoffeeLib.Window;
+// sets up a window and makes new rect and draws it
+public class TestMain {
+    static void main(String[] args) {
+
+        Window window = new Window(1000, 800, "cool window");
+        window.open();
+
+
+        Rect rect = new Rect(0.0f, 0.0f, 0.3f, 0.3f);
+        
+
+        while (!window.WindowShouldClose()){
+            window.clearBackground(0.3f, 0.5f, 0.4f, 1.0f);
+
+
+            window.draw(rect);
+
+
+            window.update();
+        }
+
+    }
+}
